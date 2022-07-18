e1190078ff4651c57d07b8dcd2d739c592c60641
Rakshan22 <100563149+Rakshan22@users.noreply.github.com>
2022-07-18 11:21:48 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,12 @@
+celebrity = input("Enter a celebrity: ")
+monument = input("Enter a monument: ")
+singular_noun = input("Enter a singular noun: ")
+animal = input("Enter an animal: ")
+liquid = input("Enter a liquid: ")
+
+print(celebrity + " is wandering home when he hears the shouts of a game from behind the, " + monument)
+print("He joins the quantam " + singular_noun)
+print("The city paints the wave function of the " + animal)
+print("New entries are allowed at " + liquid)
+
+
